f534f1d3599399bc4a1c829f30b73ab3bdbc61d9
Patryk Jeziorowski <patryk.jeziorowski@gmx.com>
2020-04-30 13:52:42 +0200

    add: node-postgresql template

---
+++ node-postgresql/app.js
@@ -0,0 +1,53 @@
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const {Pool} = require('pg');
+
+const app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+// PostgreSQL
+const pool = new Pool({
+    user: process.env.QOVERY_DATABASE_MY_POSTGRESQL_USERNAME,
+    host: process.env.QOVERY_DATABASE_MY_POSTGRESQL_HOST,
+    database: process.env.QOVERY_DATABASE_MY_POSTGRESQL_DATABASE,
+    password: process.env.QOVERY_DATABASE_MY_POSTGRESQL_PASSWORD,
+    port: process.env.QOVERY_DATABASE_MY_POSTGRESQL_PORT,
+});
+
+app.get('/', function (req, res, next) {
+    res.render('index', {title: 'Express'});
+});
+
+app.get('/users', function (req, res, next) {
+    res.send('respond with a resource 2');
+});
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+    next(createError(404));
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
+});
+
+module.exports = app;
