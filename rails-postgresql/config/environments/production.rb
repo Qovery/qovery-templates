41696594c49ad6b8d28b7371af6520f59b4b8b6e
Patryk Jeziorowski <patryk.jeziorowski@gmx.com>
2020-04-30 13:26:06 +0200

    add: rails-postgres template

--- rails-postgresql/config/environments/production.rb
+++ rails-postgresql/config/environments/production.rb
@@ -59,7 +59,7 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "qovery_rails_postgresql_production"
+  # config.active_job.queue_name_prefix = "rails_postgresql_production"
 
   config.action_mailer.perform_caching = false
 
